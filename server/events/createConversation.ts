716792b5ba0c2cc6e4426aa16476856de82255cf
Matsuel <matsuel0308@gmail.com>
2024-09-17 22:50:38 +0200

    Server createConversation -> Add user locked verification

--- server/events/createConversation.ts
+++ server/events/createConversation.ts
@@ -9,6 +9,17 @@ const createConversation = async (data: { id: string, otherId: string }, socket:
     try {
         if (await ConversationModel.findOne({ usersId: [id, otherId] })) return socket.emit('createConversation', { success: false, message: 'Conversation already exist' })
         if (await ConversationModel.findOne({ usersId: [otherId, id] })) return socket.emit('createConversation', { success: false, message: 'Conversation already exist' })
+        // si l'utilisateur courant a bloqué l'autre utilisateur, il faut le débloquer
+        const requestedUser = await UserModel.findById(id)
+        if (requestedUser) {
+            requestedUser.lockedId = requestedUser.lockedId.filter((lockedId: string) => lockedId !== otherId)
+            await requestedUser.save()
+        }
+        // si l'autre utilisateur a bloqué l'utilisateur courant, on ne peut pas créer la conversation
+        const otherUser = await UserModel.findById(otherId)
+        if (otherUser && otherUser.lockedId.includes(id)) {
+            return socket.emit('createConversation', { success: false, message: 'You are blocked by this user' })
+        }
         const conversation = new ConversationModel({
             usersId: [id, otherId],
             isGroup: false,
@@ -18,12 +29,6 @@ const createConversation = async (data: { id: string, otherId: string }, socket:
         await conversation.save()
         let conversationCollection = mongoose.model('Conversation' + conversation._id, Message)
         conversationCollection.createCollection()
-        // si l'utilisateur courant a bloqué l'autre utilisateur, il faut le débloquer
-        const requestedUser = await UserModel.findById(id)
-        if (requestedUser) {
-            requestedUser.lockedId = requestedUser.lockedId.filter((lockedId: string) => lockedId !== otherId)
-            await requestedUser.save()
-        }
         socket.emit('createConversation', { success: true })
     } catch (error) {
         console.log(error);
